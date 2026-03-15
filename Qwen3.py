5885bfc0af46a371dfdf182a31b38a676cd2ea34
Shane Uduwaka <shaneuduwaka@gmail.com>
2026-03-16 00:18:32 +0530

    Qwen3
    Setting up the environment

---
+++ Qwen3.py
@@ -0,0 +1,14 @@
+!nvidia-smi
+
+!pip install qwen-tts
+
+# Install SoX
+!apt-get install sox -y
+
+# Install FlashAttention
+#!pip install flash-attn --no-cache-dir
+
+import qwen_tts
+#import flash_attn
+print("Qwen TTS ready")
+#print("FlashAttention ready")
